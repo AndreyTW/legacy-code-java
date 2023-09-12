f3911f6255668c80d237eeeeb3ee6413cab4047c
AndreyTW <sav_687@mail.ru>
2023-09-12 16:33:05 +0300

    DeliveryControllerShould was moved from java to kotlin

--- src/test/java/ru/andreyTw/delivery/DeliveryControllerShould.kt
+++ src/test/java/ru/andreyTw/delivery/DeliveryControllerShould.kt
@@ -1,39 +1,35 @@
-package ru.andreyTw.delivery;
+package ru.andreyTw.delivery
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import ru.andreyTw.delivery.service.CostCalculationService;
-import ru.andreyTw.delivery.service.clientType.UnknownClientTypeException;
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import ru.andreyTw.delivery.service.CostCalculationService
+import ru.andreyTw.delivery.service.clientType.UnknownClientTypeException
 
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@ExtendWith(MockitoExtension.class)
-@WebMvcTest(controllers = {DeliveryController.class})
-class DeliveryControllerShould {
-
+@ExtendWith(MockitoExtension::class)
+@WebMvcTest(controllers = [DeliveryController::class])
+internal class DeliveryControllerShould {
     @MockBean
-    private CostCalculationService costCalculationServiceMock;
+    private val costCalculationServiceMock: CostCalculationService? = null
 
     @Autowired
-    private MockMvc mockMvc;
+    private val mockMvc: MockMvc? = null
 
     @Test
-    void calculateCalculatePriceForVipAndAmountOf100() throws Exception, UnknownClientTypeException {
-
-        String clientType = ClientType.VIP.getTitle();
-        Integer cartAmount = 100;
-        final ResultActions result = mockMvc.perform(get("/delivery/" + clientType + "/" + cartAmount + "/"));
-
-        result.andExpect(status().isOk());
-        verify(costCalculationServiceMock).calculate(eq(clientType), eq(cartAmount));
+    @Throws(Exception::class, UnknownClientTypeException::class)
+    fun calculatePriceForVipAndAmountOf100() {
+        val clientType = ClientType.VIP.title
+        val cartAmount = 100
+        val result = mockMvc!!.perform(get("/delivery/$clientType/$cartAmount/"))
+        result.andExpect(status().isOk())
+        verify(costCalculationServiceMock)?.calculate(eq(clientType), eq<Int>(cartAmount))
     }
 }
