cd9ee015fedfcfa07195b3c546250700b8e7a9d8
Andrey_TW <sav_687@mail.ru>
2023-09-16 23:05:58 +0300

    DeliveryStepDefinitions was moved from java to kotlin

--- src/test/java/ru/andreyTw/delivery/DeliveryStepDefinitions.kt
+++ src/test/java/ru/andreyTw/delivery/DeliveryStepDefinitions.kt
@@ -1,41 +1,39 @@
-package ru.andreyTw.delivery;
+package ru.andreyTw.delivery
 
-import io.cucumber.java.ru.Дано;
-import io.cucumber.java.ru.И;
-import io.cucumber.java.ru.Когда;
-import io.cucumber.java.ru.Тогда;
-import org.springframework.web.client.RestTemplate;
+import io.cucumber.java.ru.Дано
+import io.cucumber.java.ru.И
+import io.cucumber.java.ru.Когда
+import io.cucumber.java.ru.Тогда
+import org.junit.jupiter.api.Assertions
+import org.springframework.web.client.RestTemplate
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+class DeliveryStepDefinitions {
 
-public class DeliveryStepDefinitions {
-
-    String clientType;
-    Integer deliveryAmount;
-    Integer cartAmount;
-    Integer orderAmount = 0;
-
-    final private RestTemplate restTemplate = new RestTemplate();
+    private var clientType: String? = null
+    private var deliveryAmount: Int? = null
+    private var cartAmount: Int? = null
+    private var orderAmount: Int? = 0
+    private val restTemplate = RestTemplate()
 
     @Дано("{string} и {int}")
-    public void given(String clientType, Integer cartAmount) {
-        this.clientType = clientType;
-        this.cartAmount = cartAmount;
+    fun given(clientType: String, cartAmount: Int?) {
+        this.clientType = clientType
+        this.cartAmount = cartAmount
     }
 
     @И("Стоимость доствки {int}")
-    public void and(Integer deliveryAmount) {
-        this.deliveryAmount = deliveryAmount;
+    fun and(deliveryAmount: Int?) {
+        this.deliveryAmount = deliveryAmount
     }
 
     @Когда("Проводим расчет")
-    public void when() {
-        this.orderAmount = restTemplate.getForObject("http://localhost:8080/delivery/" + clientType + "/" + cartAmount + "/", Integer.class);
+    fun `when`() {
+        orderAmount =
+            restTemplate.getForObject("http://localhost:8080/delivery/$clientType/$cartAmount/", Int::class.java)
     }
 
     @Тогда("Получаем {int}")
-    public void then(Integer orderAmount) {
-        assertEquals(orderAmount, this.orderAmount);
+    fun then(orderAmount: Int?) {
+        Assertions.assertEquals(orderAmount, this.orderAmount)
     }
-
 }
