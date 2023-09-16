b620dbf4717610e000d8702850da6f1ce4ce5085
Andrey_TW <sav_687@mail.ru>
2023-09-16 19:04:16 +0300

    DeliveryCostCalculationService was moved from java to kotlin

--- src/main/java/ru/andreyTw/delivery/service/DeliveryCostCalculationService.kt
+++ src/main/java/ru/andreyTw/delivery/service/DeliveryCostCalculationService.kt
@@ -1,30 +1,27 @@
-package ru.andreyTw.delivery.service;
+package ru.andreyTw.delivery.service
 
-import org.springframework.stereotype.Service;
-import ru.andreyTw.delivery.ClientType;
-import ru.andreyTw.delivery.service.clientType.ClientTypeHandler;
-import ru.andreyTw.delivery.service.clientType.UnknownClientTypeException;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import org.springframework.stereotype.Service
+import ru.andreyTw.delivery.ClientType
+import ru.andreyTw.delivery.service.clientType.ClientTypeHandler
+import ru.andreyTw.delivery.service.clientType.UnknownClientTypeException
 
 @Service
-public class DeliveryCostCalculationService implements CostCalculationService {
+class DeliveryCostCalculationService(
+    handlerList: List<ClientTypeHandler?>
+) : CostCalculationService {
 
-    private final Map<ClientType, ClientTypeHandler> handlers;
+    private val handlers: Map<ClientType?, ClientTypeHandler?>
 
-    public DeliveryCostCalculationService(List<ClientTypeHandler> handlerList) {
-        handlers = handlerList.stream().collect(Collectors.toMap(ClientTypeHandler::getType, (it) -> it));
+    init {
+        handlers = handlerList.associateBy({ it?.type }, {it})
     }
 
-    @Override
-    public Integer calculate(String clientType, Integer cartAmount) throws UnknownClientTypeException {
-        return getClientDeliveryService(ClientType.titleOf(clientType)).calculate(cartAmount);
+    @Throws(UnknownClientTypeException::class)
+    override fun calculate(clientType: String?, cartAmount: Int?): Int {
+        return getClientDeliveryService(ClientType.titleOf(clientType))!!.calculate(cartAmount!!)
     }
 
-    public ClientTypeHandler getClientDeliveryService(ClientType clientType) {
-        return handlers.get(clientType);
+    fun getClientDeliveryService(clientType: ClientType): ClientTypeHandler? {
+        return handlers[clientType]
     }
-
 }
