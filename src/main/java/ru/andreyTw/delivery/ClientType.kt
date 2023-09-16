abe128c47bf50483d81f46487917acdd4eb84691
Andrey_TW <sav_687@mail.ru>
2023-09-16 19:34:25 +0300

    ClientType was moved from java to kotlin

--- src/main/java/ru/andreyTw/delivery/ClientType.kt
+++ src/main/java/ru/andreyTw/delivery/ClientType.kt
@@ -1,29 +1,22 @@
-package ru.andreyTw.delivery;
+package ru.andreyTw.delivery
 
-import ru.andreyTw.delivery.service.clientType.UnknownClientTypeException;
+import ru.andreyTw.delivery.service.clientType.UnknownClientTypeException
+import java.util.Arrays
 
-import java.util.Arrays;
-
-public enum ClientType {
+enum class ClientType(
+    val title: String
+) {
 
     COMMON("Обычный"),
     VIP("VIP"),
     FnF("Friends&Family");
 
-    private String title;
+    companion object {
 
-    ClientType(String title) {
-        this.title = title;
-    }
-
-    public static ClientType titleOf(String title) throws UnknownClientTypeException {
-        return Arrays.stream(ClientType.values())
-                .filter((ClientType clientType) -> clientType.title.equals(title))
-                .findFirst()
-                .orElseThrow(() -> new UnknownClientTypeException(title));
-    }
-
-    public String getTitle() {
-        return title;
+        @Throws(UnknownClientTypeException::class)
+        fun titleOf(title: String?): ClientType = Arrays.stream(entries.toTypedArray())
+            .filter { clientType: ClientType -> clientType.title == title }
+            .findFirst()
+            .orElseThrow { UnknownClientTypeException(title!!) }
     }
 }
